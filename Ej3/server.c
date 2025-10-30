fa01d57d5de8e11c1f15a1bb771d3e65344b54cd
Tomas Vecchi <204607402+tvecchiUTN@users.noreply.github.com>
2025-10-30 09:44:59 -0300

    Mejoras con los hilos que hablan

--- Ej3/server.c
+++ Ej3/server.c
@@ -13,53 +13,92 @@ typedef struct
 
 volatile int crtNew = 1;
 
-void* hiloDialogo(void* args)
-{
-    int fd = *(int*)args;
+pthread_mutex_t myMutex;
 
-    while(1)
+void *hiloDialogo(void *args)
+{
+    int fd = *(int *)args;
+
+    int flagCreate = 1;
+
+    while (1)
     {
         int littleSk = WaitConnect(fd);
 
-        printf("Conectado\n");
-        crtNew = 1;
-        
+        if (flagCreate)
+        {
+            pthread_mutex_lock(&myMutex);
+            crtNew = 1;
+            pthread_mutex_unlock(&myMutex);
+            flagCreate = 0;
+        }
+
         send(littleSk, "Prueba 1 2 3", 13, 0);
 
-        sleep(10);
+        sleep(60);
 
         CloseConnect(littleSk);
     }
+
+    pthread_exit(NULL);
 }
 
 int main(int argc, char **argv)
 {
     int puerto;
-    if(checkPuerto(argc, argv, &puerto) < 0)
+    if (checkPuerto(argc, argv, &puerto) < 0)
     {
         fprintf(stderr, "Error de argumentos\n");
         return ERR;
     }
 
-    pthread_t *hilos = malloc(10 * sizeof(pthread_t));
+    int capMax = 16;
+    pthread_t *hilos = malloc(capMax * sizeof(pthread_t));
     int szHil = 0;
-    if(!hilos)
+    int flagMax = 0;
+
+    if (!hilos)
     {
         fprintf(stderr, "Error vector de hilos\n");
         return ERR;
     }
 
+    pthread_mutex_init(&myMutex, NULL);
+
     int mainSK = OpenServer(puerto);
 
-    while(1)
+    while (1)
     {
-        if(crtNew)
+        if (szHil >= capMax) // Seccion de reajuste de hilos
         {
-            pthread_create(hilos+szHil, NULL, hiloDialogo, &mainSK);
-            szHil++;
-            crtNew = 0;
+            capMax *= 2;
+            pthread_t *auxHilos = realloc(hilos, capMax * sizeof(pthread_t));
+            if (!auxHilos)
+            {
+                printf("No se pudo reservar mas memoria\n");
+                flagMax = 1;
+            }
+            else
+            {
+                hilos = auxHilos;
+            }
         }
-        sleep(1);
-    }
 
+        if (crtNew) //Si puedo crar un nuevo hilo
+        {
+            if (!flagMax) //EN caso que no pueda guardar mas
+            {
+                pthread_create(hilos + szHil, NULL, hiloDialogo, &mainSK);
+                pthread_detach(hilos + szHil);
+                printf("Se creo un hilo para escuchar\n");
+                szHil++;
+
+                pthread_mutex_lock(&myMutex);
+                crtNew = 0;
+                pthread_mutex_unlock(&myMutex);
+            }
+        }
+
+        sleep(1); //Reducir la cantidad de procesos
+    }
 }
